1cc82c24132799854634713d4012a901c4e55daa
Aldinei <aldinei.franco@unimedprudente.com.br>
2025-03-11 16:11:35 -0300

    upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import PopoverDemo from './components/Popover'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <PopoverDemo />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Popover/index.tsx
@@ -0,0 +1,15 @@
+import { Popover } from "radix-ui";
+
+const PopoverDemo = () => (
+  <Popover.Root>
+    <Popover.Trigger>More info</Popover.Trigger>
+    <Popover.Portal>
+      <Popover.Content>
+        Some more info…
+        <Popover.Arrow />
+      </Popover.Content>
+    </Popover.Portal>
+  </Popover.Root>
+);
+
+export default PopoverDemo;
