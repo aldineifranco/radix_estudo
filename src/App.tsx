e94211b249ed66cb17f9c6ef1bcf3707abfa3ce9
aldinei <aldineifranco@gmail.com>
2025-03-12 22:32:08 -0300

    componentes box, flex, text e section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import PopoverDemo from './components/Popover'
-import './App.css'
+// import PopoverDemo from './components/Popover'
+import './index.css'
+import { Flex, Text, Button, Container, Section, Box } from '@radix-ui/themes'
+import "@radix-ui/themes/styles.css";
 
 function App() {
 
   return (
     <>
-      <PopoverDemo />
+      <Box py="2" className='bg-cyan-900'>
+        <Flex direction="column" gap="4">
+          <Text>Hello from Radix Themes</Text>
+        </Flex>
+      </Box>
+      <Section>
+        <Button>Let's go</Button>
+      </Section>
     </>
+ 
+
   )
 }
 
